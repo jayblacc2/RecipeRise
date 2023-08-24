f47c6f4be5c087142aff814ab3000259515b1769
jayblacc2 <jideman2k@yandex.ru>
2023-08-24 13:40:40 +0400

    feature icon icon save

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+const headingElements = document.querySelectorAll('h1,h2,h3');
 const searchContent = document.querySelector('.search-img');
 const showContent = document.querySelector('.search__icon-content');
 const secNavContent = document.querySelector('.secondary__nav-contents');
 const closeContent = document.querySelector('#close__search-content');
+const overlayWindow = document.querySelector('.overlay');
+const modal = document.querySelector('.modal-window');
+
+const closeButtons = document.querySelectorAll('.close-btn');
+const toggleButton = document.querySelector('.modal__btn-toggle');
+
+const dropdowns = document.querySelectorAll('.dropdown__content');
+
 const secNavList = document.querySelector(
   '.secondary__nav-contents'
 ).firstElementChild;
 
 searchContent.addEventListener('click', (e) => {
-  e.preventDefault();
   showContent.classList.remove('visually-hidden');
   secNavList.style.opacity = 0;
 });
@@ -17,4 +25,28 @@ closeContent.addEventListener('click', () => {
   secNavList.style.opacity = 1;
 });
 
-console.log(secNavList);
+headingElements.forEach((heading) => {
+  heading.addEventListener('mouseover', (e) => {
+    console.log('FIX HERE');
+  });
+});
+
+toggleButton.addEventListener('click', (e) => {
+  overlayWindow.classList.remove('visually-hidden');
+  setTimeout(() => {
+    modal.classList.remove('visually-hidden');
+  }, 200);
+});
+
+closeButtons.forEach((closeButton) => {
+  closeButton.addEventListener('click', () => {
+    overlayWindow.classList.add('visually-hidden');
+    modal.classList.add('visually-hidden');
+  });
+});
+
+//dropdown
+dropdowns.forEach((dropdown) => {
+  const parentLink = dropdown.parentNode.querySelector('.nav__link');
+  const dropdownItemText = parentLink.textContent.trim();
+});
